083212cec4177c3b90e942df85ed52a698d70035
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-07-16 16:02:23 +0200

    feat(main.cpp): add input validation for PmergeMe
    This commit introduces input validation for the PmergeMe program. It
    checks if the program receives at least two arguments, and validates
    that each argument is a positive integer not exceeding the maximum
    value for an integer. If these conditions are not met, the program
    outputs an error message and exits with a failure status.

---
+++ ex02/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:53:41 by jmigoya-          #+#    #+#             */
+/*   Updated: 2024/07/16 15:13:55 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+int main(int ac, char *av[])
+{
+	if (ac <= 2)
+	{
+		std::cout
+			<< "Error: PmergeMe takes at least two positive integers as arguments."
+			<< std::endl;
+		return EXIT_FAILURE;
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		if (std::strlen(av[i]) > 10 || std::strcmp(av[i], "2147483647") > 0)
+		{
+			std::cout << "Error." << std::endl;
+			return EXIT_FAILURE;
+		}
+		for (int j = 0; av[i][j] != '\0'; j++)
+		{
+			if (!std::isdigit(av[i][j]))
+			{
+				std::cout << "Error." << std::endl;
+				return EXIT_FAILURE;
+			}
+		}
+	}
+	return 0;
+}
